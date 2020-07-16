e4ac670a6a122782302290ed5829d7eed5b8f5e7
Meghann Silagan <meghannsilagan@Meghanns-MBP.hsd1.il.comcast.net>
2020-07-16 01:06:17 -0500

    attempt at connection

--- server/server.js
+++ server/server.js
@@ -1,5 +1,3 @@
-'use strict';
-
 const express = require('express');
 
 // Constants
@@ -9,7 +7,38 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.send('Welcome Home');
+});
+
+app.get('/about', (req, res) => {
+  res.send('Just a kween in search of her throne');
+});
+
+app.get('/experience', (req, res) => {
+  res.send('no experiences here yet!');
+});
+
+app.get('/skills', (req, res) => {
+  res.send('no experiences here yet!');
+});
+
+app.get('/projects', (req, res) => {
+  res.send('no experiences here yet!');
+});
+
+app.get('/leadership', (req, res) => {
+  res.send('no experiences here yet!');
+});
+
+app.get('/passions', (req, res) => {
+  res.send('tacos and the end of white supremacy!');
+});
+
+app.post('/holla', (req, res) => {
+  console.log(req.body);
+  res.send(
+    `I received your POST request. This is what you sent me: ${req.body.post}`,
+  );
 });
 
 app.listen(PORT, HOST);
